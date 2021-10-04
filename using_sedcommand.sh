3edc4b2e4c955873c731dba3b58fc5b1938ae763
vishal <vishalrathod7038@gmail.com>
2021-10-04 23:09:00 +0530

    updated array using sedcommand

---
+++ using_sedcommand.sh
@@ -0,0 +1,17 @@
+ vishal rathod
+ amol ade
+ aksh pawar
+yogesh maske
+ ajay shinde
+ amar chavan
+ apul mane
+ vijay jadhao
+
+ rathod   amol
+  ade    yogesh 
+ pawar    vijay
+ maske     vishal
+  shinde   apul
+ chavan    vijay
+ mane     amol
+  jadhao  vishal
